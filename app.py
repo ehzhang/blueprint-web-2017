1ef597d8b44528a62d001ee369d6db954ebba3f1
Katie Siegel <katiesiegel@samsara.com>
2017-02-15 23:41:16 -0800

    app.py: Implement leaderboard class and leaderboard update request

--- app.py
+++ app.py
@@ -1,11 +1,53 @@
+#######################################
+# Imports
+#######################################
 from flask import Flask
 from flask import render_template
 from flask import request
+from operator import itemgetter
+import json
+
+######################################
+# App initialization
+#####################################
 app = Flask(__name__)
+
+####################################
+# Leaderboard
+####################################
+class Leaderboard:
+    def __init__(self):
+        # scores contains tuples of (name, score)
+        # ordered by score (maximum to minimum)
+        self.scores = []
+        self.capacity = 10 # maximum leaderboard capacity
+
+    def submit_score(self, name, score):
+        self.scores.append((name, score))
+        self.scores = sorted(self.scores, key=itemgetter(1), reverse=True)
+        if len(self.scores) > self.capacity:
+            # Remove lowest score if over capacity
+            self.scores.pop(self.capacity)
+
+leaderboard = Leaderboard()
+
+###################################
+# Routes
+###################################
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/scores", methods=["POST"])
+def scores():
+    # Extract data from request
+    print "here"
+    print request.json
+    name = request.json["name"]
+    score = request.json["score"]
+    leaderboard.submit_score(name, score)
+    return json.dumps({"status": "OK", "scores": leaderboard.scores})
 
 if __name__ == "__main__":
     app.run()
