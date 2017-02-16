977c967c4d5ede8fc977d6b1657183e3bc4ea532
Edwin Zhang <ehzhang@alum.mit.edu>
2017-02-15 22:35:59 -0800

    hello blueprint

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask import render_template
+from flask import request
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello World!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
