1103464aa1a5a955031c28a79d2d238db4294dc3
Katie Siegel <katiesiegel@samsara.com>
2017-02-27 23:00:49 -0800

    static/main.js: remove AJAX code

--- static/main.js
+++ static/main.js
@@ -167,33 +167,4 @@
   game.state.add('main', mainState);
 
   game.state.start('main');
-
-  $(document).ready(function(){
-    $('#restart-button').click(function(){
-      game.state.states.main.restartGame();
-    });
-    $("#submit-button").click(function(){
-      $.ajax({
-        url: "/scores",
-        type: "POST",
-        data: JSON.stringify({
-          name: $('#name').val(),
-          score: GAME_SCORE
-        }),
-        dataType: "json",
-        contentType: "application/json"
-      })
-      .done(function (data) {
-        // TODO update leaderboard
-        console.log(data);
-
-        var boardContent = data.scores.map(function(score){
-          return "<li><div class='name'>" + score[0] + "</div><div class='score'>" + score[1] + "</div></li>";
-        }).join('');
-
-        $('.leaderboard .scores').html(boardContent);
-
-      });
-    });
-  });
 })();
